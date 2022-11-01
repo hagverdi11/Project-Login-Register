567c00a844fb9d6e22872cdba7f392c723c9ab33
hagverdi11 <hagverdiam@code.edu.az>
2022-11-01 16:09:07 +0400

    Added task

---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EmbryoFrontToBack.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbryoFrontToBack.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser> 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Accessories> Accessories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Migrations/20221101111710_AddActiveColumnToUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmbryoFrontToBack.Migrations
+{
+    public partial class AddActiveColumnToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isActive",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isActive",
+                table: "AspNetUsers");
+        }
+    }
+}
